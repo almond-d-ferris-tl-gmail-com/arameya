c2f6fee6f5352d0e7ce0c4dfc4c66fd91dd16837
EC2 Default User <ec2-user@ip-172-31-16-252.ap-northeast-1.compute.internal>
2022-12-01 14:09:13 +0000

    [20221201Add]arameya admin review一覧、詳細、削除

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,9 +1,22 @@
 class Admin::ReviewsController < ApplicationController
   def index
-    @review = Review.find(params[:id])
+    # 会員の情報を取得
+    @member = Member.find(params[:member_id])
+    # 会員が投稿したレビューの情報を取得
+    @reviews = @member.reviews
+    # 会員名などを表示させないなら、「@reviews = Member.find(params[:member_id]).reviews」とまとめてOK
   end
 
   def show
+    @member = Member.find(params[:member_id])
     @review = Review.find(params[:id])
+    @order = Order.find(params[:id])
   end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to admin_member_reviews_path(params[:member_id]) #admin/reviews#indexへのpath
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,73 +1,76 @@
 Rails.application.routes.draw do
 
-# 会員用
-# URL /members/sign_in ...
-devise_for :members, skip: [:passwords], controllers: {
-registrations: "public/registrations",
-sessions: 'public/sessions'
-}
+    # 会員用
+    # URL /members/sign_in ...
+    devise_for :members, skip: [:passwords], controllers: {
+        registrations: "public/registrations",
+        sessions: 'public/sessions'
+    }
+    
+    # 管理者用
+    # URL /admin/sign_in ...
+    devise_for :admin, skip: [:registrations, :passwords], controllers: {
+        sessions: "admin/sessions"
+    }
+    
+    # 会員
+    scope module: :public do
+        #namespace→scope moduleに変更
+        #URLは変えず、ファイル構成だけ指定のパスにする(フォルダ名にはpublicをつけて、URLにはつけない)
+        
+        resources :reviews, only: [:index, :new, :show, :edit, :create, :update, :destroy]
+        
+        get '/search', to: 'searches#search'
+        
+        resources :sell_order_details, only: [:update]
+        
+        resources :sell_orders, only: [:index, :show, :update]
+        
+        resources :sell_items, only: [:index, :new, :show, :edit, :create, :update, :destroy]
+        
+        # orders#showよりも上に記述しないとcompleteがid扱いされる
+        post 'buy_orders/comfirm'
+        get 'buy_orders/complete'
+        resources :buy_orders, only: [:new, :index, :show, :create]
+        
+        resources :cart_items, only: [:index, :create, :update, :destroy]
+        
+        resources :buy_items, only: [:index, :show]
+        
+        resources :buy_addresses, only: [:index, :edit, :create, :update, :destroy]
+        
+        # membersはURLを変更するのでresourcesは使えない
+        # show 会員のマイページ
+        get 'members/my_page', to: 'members#show'
+        # edit 会員の登録情報編集
+        get 'members/information/edit', to: 'members#edit'
+        # 会員の退会確認(URL変更なし)
+        get 'members/unsubscribe'
+        # 会員の退会完了(URL変更なし)
+        get 'members/complete'
+        # update 会員の登録情報更新
+        patch '/members/information', to: 'members#update'
+        # 会員の退会処理(ステータスの更新)(URL変更なし)
+        patch '/members/withdraw'
+        
+        # 'URL' => 'コントローラ#アクション', as: :Prefix(パスが入った変数)の変更
+        get '/' => 'homes#top', as: :root
+        get '/about' => 'homes#about'
+    end
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords], controllers: {
-sessions: "admin/sessions"
-}
-
-# 会員
-scope module: :public do
-#namespace→scope moduleに変更
-#URLは変えず、ファイル構成だけ指定のパスにする(フォルダ名にはpublicをつけて、URLにはつけない)
-
-resources :reviews, only: [:index, :new, :show, :edit, :create, :update, :destroy]
-
-get '/search', to: 'searches#search'
-
-resources :sell_order_details, only: [:update]
-
-resources :sell_orders, only: [:index, :show, :update]
-
-resources :sell_items, only: [:index, :new, :show, :edit, :create, :update, :destroy]
-
-# orders#showよりも上に記述しないとcompleteがid扱いされる
-post 'buy_orders/comfirm'
-get 'buy_orders/complete'
-resources :buy_orders, only: [:new, :index, :show, :create]
-
-resources :cart_items, only: [:index, :create, :update, :destroy]
-
-resources :buy_items, only: [:index, :show]
-
-resources :buy_addresses, only: [:index, :edit, :create, :update, :destroy]
-
-# membersはURLを変更するのでresourcesは使えない
-# show 会員のマイページ
-get 'members/my_page', to: 'members#show'
-# edit 会員の登録情報編集
-get 'members/information/edit', to: 'members#edit'
-# 会員の退会確認(URL変更なし)
-get 'members/unsubscribe'
-# 会員の退会完了(URL変更なし)
-get 'members/complete'
-# update 会員の登録情報更新
-patch '/members/information', to: 'members#update'
-# 会員の退会処理(ステータスの更新)(URL変更なし)
-patch '/members/withdraw'
-
-# 'URL' => 'コントローラ#アクション', as: :Prefix(パスが入った変数)の変更
-get '/' => 'homes#top', as: :root
-get '/about' => 'homes#about'
-end
-
-# 管理者
-namespace :admin do
-resources :genres, only: [:index, :edit, :create, :update, :destroy]
-resources :member_sells, only: [:index, :show]
-resources :member_buys, only: [:index, :show]
-resources :members, only: [:show, :edit, :update]
-resources :reviews, only: [:index, :show]
-
-get '/' => 'homes#top'
-end
+    # 管理者
+    namespace :admin do
+        resources :genres, only: [:index, :edit, :create, :update, :destroy]
+        resources :member_sells, only: [:index, :show]
+        resources :member_buys, only: [:index, :show]
+        # 「会員個人が行なったレビュー・評価」を表示するためにmembersの中にreviewsを入れる(do-endで括る)
+        # フォルダ構成はそのままだが、URLとprefixが変わる
+        resources :members, only: [:show, :edit, :update] do
+            resources :reviews, only: [:index, :show, :destroy]
+        end
+        get '/' => 'homes#top'
+    
+    end
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 # resourcesメソッド:railsで定義されている7つのアクションのルーティングを自動で作成する
