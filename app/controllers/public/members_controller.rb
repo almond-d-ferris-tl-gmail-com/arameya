0bc5f25aa1c269492f71a9a6f91bd8b4877d8273
EC2 Default User <ec2-user@ip-172-31-16-252.ap-northeast-1.compute.internal>
2023-01-09 13:05:38 +0000

    [20230109Fix]arameya public member_controller修正

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,7 +1,7 @@
 class Public::MembersController < ApplicationController
   # ログインしていない場合、ヘッダーのボタンをクリックしたら強制的にログイン画面に移動する
   # except→ログイン画面への遷移を除外する→今回は除外するものがない
-  # before_action :authenticate_member!
+   before_action :authenticate_member!
 
   def show
     # マイページをクリックしたらprivateのcreate_room(管理者ー会員メッセージルーム)を作成する
